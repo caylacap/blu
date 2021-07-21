f1dd49191b498a51474cebb2a37759f468ac9c55
Sarah Sattar <sarahsattar724@gmail.com>
2021-07-21 11:19:12 -0400

    blucare started

--- blu/BlucareViewController.swift
+++ blu/BlucareViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class BlucareViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelTip: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        let array = ["Close your eyes. Inhale for four seconds, exhale for eight. Repeat this five times, or more if you'd like.", "Get out your yoga mat and let's do some yoga!", "Take a warm bath","Drink a glass of cold water", "Make some tea- chamomile?"]
+        
+        labelTip.text = array.randomElement()
+    }
+    
     /*
     // MARK: - Navigation
 
