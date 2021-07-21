ab63e78d9f928c089565571007d20ffe38b91c6e
Sarah Sattar <sarahsattar724@gmail.com>
2021-07-21 13:48:52 -0400

    bluster fixed

--- blu/BlusterViewController.swift
+++ blu/BlusterViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class BlusterViewController: UIViewController {
 
+    @IBOutlet weak var labelBoost: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        let words = ["You're doing great today! Keep on killing it!", "You are gorgeous!", "You are loved by so many", "You got this! Keep going!", "You amaze me everyday!","Can't stop, won't stop, don't stop!","There are so many people in this world who care about you that you may not even know about.","Someone is thinking of you right now.", "Take a moment for yourself, because you should be your favorite person."]
+        labelBoost.text = words.randomElement()
 
+    }
+    
     /*
     // MARK: - Navigation
 
